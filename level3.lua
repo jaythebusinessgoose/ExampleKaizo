12069f8a13205e4de0f0e60311a7493520c85048
JayTheBusinessGoose <jaythebusinessgoose@gmail.com>
2024-07-20 14:02:13 -0600

    Add some levels and a shortcut.

---
+++ level3.lua
@@ -0,0 +1,40 @@
+define_tile_code("catmummy")
+
+local LevelSequence = require('LevelSequence.level_sequence')
+local BORDER_THEME = LevelSequence.BORDER_THEME
+
+local level2 = {
+    identifier = "level3",
+    title = "Level 3",
+    theme = THEME.VOLCANA,
+    border_theme = THEME.COSMIC_OCEAN,
+    loops = true,
+    background_theme = THEME.ICE_CAVES,
+    music_theme = THEME.DUAT,
+    file_name = "level3.lvl",
+    width = 4,
+    height = 4,
+}
+
+local level_state = {
+    loaded = false,
+    callbacks = {},
+}
+
+level2.load_level = function()
+    if level_state.loaded then return end
+    level_state.loaded = true
+end
+
+level2.unload_level = function()
+    if not level_state.loaded then return end
+
+    local callbacks_to_clear = level_state.callbacks
+    level_state.loaded = false
+    level_state.callbacks = {}
+    for _, callback in pairs(callbacks_to_clear) do
+        clear_callback(callback)
+    end
+end
+
+return level2
--- main.lua
+++ main.lua
@@ -10,9 +10,19 @@ local SIGN_TYPE = level_sequence.SIGN_TYPE
 
 local level1 = require('level1')
 local level2 = require('level2')
+local level3 = require('level3')
+local level4 = require('level4')
+local level5 = require('level5')
 
-level_sequence.set_levels({level1, level2})
+level_sequence.set_levels({level1, level2, level3, level4, level5})
 
+define_tile_code("shortcut")
+set_pre_tile_code_callback(function(x, y, layer)
+    level_sequence.spawn_shortcut(x, y, layer, level3, SIGN_TYPE.RIGHT)
+    return true
+end, "shortcut")
+
+level_sequence.set_hide_entrance(false)
 
 level_sequence.set_on_win(function(attempts, total_time)
     print("You won!")
